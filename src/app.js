05d53d07054d35baa75c5a838b5a23e11fbd618c
Tijani Fulani <Fulani07@gmail.com>
2021-07-22 15:25:23 +0100

    server and man route up and running

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 // Imports
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const BreweryRouter = require('./resources/breweries/routes');
 // Initialising server
 const app = express();
 
 // Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Routes
+app.use('/breweries', BreweryRouter);
 
 // Port
 const port = 4000;
